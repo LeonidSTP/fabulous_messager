a3b1df1e29c46fbeff95cb59587a1368db698fe4
Olexiy Tsymbalyuk <olexiy.tsymbalyuk@gmail.com>
2017-11-07 18:20:22 +0200

    basic routing

--- .angular2cli/src/app/app.module.ts
+++ .angular2cli/src/app/app.module.ts
@@ -1,18 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 import { AuthFormComponent } from './components/auth-form/auth-form.component';
 
+const appRoutes: Routes = [
+    {path: 'auth', component: AuthFormComponent},
+];
+
 @NgModule({
-  declarations: [
-    AppComponent,
-    AuthFormComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        AuthFormComponent
+    ],
+    imports: [
+        BrowserModule,
+        RouterModule.forRoot(
+            appRoutes
+        )],
+    providers: [],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
