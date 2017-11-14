08704684275e4f5cab8d66535fa059a2d2290558
pogodich <pogodich1@gmail.com>
2017-11-14 18:45:29 +0200

    footer styles fixed

--- .angular2cli/src/app/app.module.ts
+++ .angular2cli/src/app/app.module.ts
@@ -4,7 +4,8 @@ import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 import { NavBarModule } from './components/Navbar/navbar.module';
 import { AuthFormComponent } from './components/auth-form/auth-form.component';
-import { MyfooterComponent } from "./components/MyFooter/myfooter.component";
+import { MyfooterComponent } from './components/MyFooter/myfooter.component';
+import { MessageModule } from './components/Message/message.module';
 
 const appRoutes: Routes = [
     {path: 'auth', component: AuthFormComponent},
@@ -19,6 +20,7 @@ const appRoutes: Routes = [
     imports: [
         BrowserModule,
         NavBarModule,
+        MessageModule,
         RouterModule.forRoot(
             appRoutes
         )],
