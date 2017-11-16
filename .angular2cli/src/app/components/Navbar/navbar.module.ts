e8a1bba60266511e253ed239a639da1f30534a0c
singithub@gmail.com <anyway1997>
2017-11-16 23:04:01 +0200

    routers

--- .angular2cli/src/app/components/Navbar/navbar.module.ts
+++ .angular2cli/src/app/components/Navbar/navbar.module.ts
@@ -1,14 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavBarComponent } from './navbar.component';
+import { RouterModule, Routes } from '@angular/router';
 
 @NgModule({
-  imports: [
-    CommonModule,
-  ],
-  exports: [NavBarComponent],
-  declarations: [NavBarComponent],
-  providers: []
+    imports: [
+        CommonModule,
+        RouterModule
+    ],
+    exports: [NavBarComponent],
+    declarations: [NavBarComponent],
+    providers: []
 })
 
 export class NavBarModule {
