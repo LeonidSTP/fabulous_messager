8d4fb7e909517a4ac9996620126b40abecd5990e
pogodich <pogodich1@gmail.com>
2017-11-20 23:06:19 +0200

    resolve conflicts

--- .angular2cli/src/app/app.module.ts
+++ .angular2cli/src/app/app.module.ts
@@ -1,21 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
-import { NavBarModule } from './components/Navbar/navbar.module';
+import { NavBarComponent } from './components/Navbar/navbar.component';
 import { AuthFormComponent } from './components/auth-form/auth-form.component';
 import { MyfooterComponent } from './components/MyFooter/myfooter.component';
 import { appRouterModule } from './app.routers';
+import { MessageModule } from './components/Message/message.module';
 
 @NgModule({
     declarations: [
         AppComponent,
+        NavBarComponent,
         AuthFormComponent,
         MyfooterComponent
     ],
     imports: [
         BrowserModule,
-        NavBarModule,
-        appRouterModule
+        appRouterModule,
+        MessageModule
     ],
     providers: [],
     bootstrap: [AppComponent],
--- .angular2cli/src/app/components/Message/message.module.ts
+++ .angular2cli/src/app/components/Message/message.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import {MessageComponent} from './message.component';
-import { FormsModule } from '@angular/forms'
+import { MessageComponent } from './message.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
     imports: [
