69ad42aaefd147aa0c6bec99a3d56915020ad8d5
Leonid <leonardodestakhov@gmail.com>
2017-11-21 19:58:53 +0200

    upgrade message

--- .angular2cli/src/app/components/Message/message.component.ts
+++ .angular2cli/src/app/components/Message/message.component.ts
@@ -7,11 +7,18 @@ import { NgModel } from '@angular/forms';
     templateUrl: 'message.html'
 })
 export class MessageComponent {
-    public messages: string[] = [];
-    public message: string;
+    public messages: any[] = [];
+    public message: any;
 
     public addMessage(): void {
-      this.messages.push(this.message);
+      const myMessage = {
+        text: this.message,
+        likes: 0,
+        dislikes: 0,
+      };
+
+      this.messages.push(myMessage);
+      console.log(myMessage);
       delete this.message;
 
 
