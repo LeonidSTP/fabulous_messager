e872d98baf07b3db9d6205ad5feecb2ce8958388
Olexiy Tsymbalyuk <olexiy.tsymbalyuk@gmail.com>
2017-11-21 17:42:21 +0200

    flexible form

--- .angular2cli/src/app/components/auth-form/auth-form.component.ts
+++ .angular2cli/src/app/components/auth-form/auth-form.component.ts
@@ -1,13 +1,37 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
-  selector: 'app-auth-form',
-  templateUrl: './auth-form.component.html',
-  styleUrls: ['./auth-form.component.less']
+    selector: 'app-auth-form',
+    templateUrl: './auth-form.component.html',
+    styleUrls: ['./auth-form.component.less']
 })
 export class AuthFormComponent implements OnInit {
 
-  ngOnInit() {
-  }
+    public currentUrl: string;
 
+    constructor(private router: Router) {
+
+    }
+
+
+    ngOnInit() {
+        this.currentUrl = this.router.url;
+    }
+
+    public proccessLogin = () => {
+        if (this.currentUrl === '/auth') {
+            this.login();
+        } else {
+            this.signup();
+        }
+    }
+
+    public login = () => {
+        console.log('login');
+    }
+
+    public signup = () => {
+        console.log('signup');
+    }
 }
