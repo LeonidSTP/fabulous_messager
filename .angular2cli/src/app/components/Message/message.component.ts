edd3b57ef43b4b7108bf0f5c869135b002dce8c2
Leonid <leonardodestakhov@gmail.com>
2017-11-27 22:40:01 +0200

    upgrade message

--- .angular2cli/src/app/components/Message/message.component.ts
+++ .angular2cli/src/app/components/Message/message.component.ts
@@ -11,16 +11,38 @@ export class MessageComponent {
     public message: any;
 
     public addMessage(): void {
-      const myMessage = {
-        text: this.message,
-        likes: 0,
-        dislikes: 0,
-      };
-
+    const myMessage = {
+      text: this.message,
+      likes: 0,
+      dislikes: 0,
+      Islike: false,
+      Dislike: false,
+    };
       this.messages.push(myMessage);
       console.log(myMessage);
       delete this.message;
 
+  }
+  public Like(myMessage){
+      if(!myMessage.Islike) {
+        if(myMessage.Dislike){
+          myMessage.dislikes--;
+          myMessage.Dislike = false;
+        }
+        myMessage.likes++;
+        myMessage.Islike = true;
+      }
 
+  }
+  public disLike(myMessage){
+    if(!myMessage.Dislike) {
+      if(myMessage.Islike){
+        myMessage.likes--;
+        myMessage.Islike = false;
+      }
+      myMessage.dislikes++;
+      myMessage.Dislike = true;
     }
+  }
+
 }
