5c0f76e1c84fc09f7eb8a169a1e714c4165f4231
Pogodich <pogodich1@gmail.com>
2018-03-12 22:35:04 +0200

    sample api request to save user in mongo

--- server.js
+++ server.js
@@ -1,14 +1,31 @@
 const express = require('express');
 const logger = require('morgan');
-const bodyParser = require('body-parser')
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose'); //ORM allow access to MongoDB
+const config = require('./config/config');
 const app = express();
-app.use( bodyParser.json() );       // to support JSON-encoded bodies
+app.use(bodyParser.json());       // to support JSON-encoded bodies
 app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
-  extended: true
+    extended: true
 }));
+//Set up default mongoose connection
+mongoose.connect(config.mongoUrl);
+//Get the default connection
+const db = mongoose.connection;
+// Get Mongoose to use the global promise library
+mongoose.Promise = global.Promise;
+
+db.on('error', (err) => {
+    console.error('connection error:', err.message);
+});
+db.once('open', () => {
+    console.info('Connected to DB');
+});
+
+const userModel = require('./models/user');
 
 app.use(logger('dev'));
-app.listen(3000,  () => {
+app.listen(3000, () => {
     console.log('Example app listening on port 3000!')
 });
 
@@ -21,7 +38,12 @@ app.use(function (req, res, next) {
 });
 
 app.post('/login', (req, res) => {
-    const user = req.body;
-    console.log(user);
-    res.status(200).json(user);
+    const user = new userModel(req.body);
+    user.save((error,  createdUser) => {
+        if (error) {
+            res.send(500, error.message)
+        }
+        console.log('New User Created--->', createdUser);
+        res.status(200).json(createdUser);
+    });
 });
