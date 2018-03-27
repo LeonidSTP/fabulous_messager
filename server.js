4b32c9592e95e9342877b932d2ef445c03830a2b
vlad <vbvvlad@gmail.com>
2018-03-27 18:23:56 +0300

    Merge branches 'development' and 'password-encoding' of https://github.com/redpanda8814/fabulous_messager into password-encoding
    # Conflicts:
    #	.angular2cli/package-lock.json
    #	.angular2cli/src/app/app.module.ts
    #	.angular2cli/src/app/components/auth-form/auth-form.component.ts
    #	package-lock.json
    #	package.json

--- server.js
+++ server.js
@@ -3,15 +3,10 @@ const logger = require('morgan');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose'); //ORM allow access to MongoDB
 const config = require('./config/config');
-const jwt  = require('jsonwebtoken');
+const jwt = require('jsonwebtoken');
 const app = express();
 const jwtconf = require('./jwt');
 
-
-//////////////////
-
-
-//////////////////////
 app.use(bodyParser.json());       // to support JSON-encoded bodies
 app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
     extended: true
@@ -43,57 +38,15 @@ app.use(function (req, res, next) {
     next();
 });
 
-app.post('/login', (req, res) => {
 
-    const user = new userModel(req.body);
-    user.save((error, createdUser) => {
-        if (error) {
-            res.send(500, error.message)
-        }
-        res.status(200).json(createdUser);
-    });
-});
-
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
     console.log('Middleware');
     next()
 });
 
-route middleware to verify a token
-app.use(function(req, res, next) {
-
-  // check header or url parameters or post parameters for token
-  const token = req.body.token || req.query.token || req.headers['x-access-token'];
-
-  // decode token
-  if (token) {
-
-    // verifies secret and checks exp
-    jwt.verify(token, app.get('superSecret'), function(err, decoded) {
-      if (err) {
-        return res.json({ success: false, message: 'Failed to authenticate token.' });
-      } else {
-        // if everything is good, save to request for use in other routes
-        req.decoded = decoded;
-
-        next();
-      }
-    });
-
-  } else {
-
-    // if there is no token
-    // return an error
-    return res.status(403).send({
-        success: false,
-        message: 'No token provided.'
-    });
-
-  }
-});
 
 app.post('/register', (req, res) => {
-    if(!req.body.userName){
+    if (!req.body.userName) {
         res.status(500).send("UserName not provided");
     }
     const user = new userModel(req.body);
@@ -106,51 +59,83 @@ app.post('/register', (req, res) => {
     });
 });
 
-app.get('/users', function(req, res) {
+app.get('/users', function (req, res) {
     console.log('Test');
-    userModel.find({}, function(err, user) {
-    res.json(user);
-  });
+    userModel.find({}, function (err, user) {
+        res.json(user);
+    });
 });
 
 app.set('superSecret', jwtconf.secret); // secret variable
-app.post('/authenticate', function(req, res) {
 
-  // find the user
-  userModel.findOne({  userName: req.body.name }, function(err, user) {
 
-    if (err) throw err;
+app.post('/login', function (req, res) {
+    console.log(req.body);
+    // find the user
+    userModel.findOne({email: req.body.email}, function (err, user) {
 
-    if (!user) {
-      res.json({ success: false, message: 'Authentication failed. User not found.' });
-    } else if (user) {
-      // check if password matches
-      if (user.password != req.body.password) {
-        res.json({ success: false, message: 'Authentication failed. Wrong password.' });
-      } else {
-        // if user is found and password is right
-        // create a token with only our given payload
-    // we don't want to pass in the entire user since that has the password
-    const payload = {
-      email: user.email
-    };
-        var token = jwt.sign(payload, app.get('superSecret'), {
-            expiresIn: '24h' // expires in 24 hours
-        });
+        if (err) throw err;
 
-        // return the information including token as JSON
-        res.json({
-          success: true,
-          message: 'Enjoy your token!',
-          token: token
-        });
-      }
-    }
+        if (!user) {
+            res.json({success: false, message: 'Authentication failed. User not found.'});
+        } else if (user) {
+            // check if password matches
+            if (user.password != req.body.password) {
+                res.json({success: false, message: 'Authentication failed. Wrong password.'});
+            } else {
+                // if user is found and password is right
+                // create a token with only our given payload
+                // we don't want to pass in the entire user since that has the password
+                const payload = {
+                     userName: user.userName
+                };
+                const token = jwt.sign(payload, app.get('superSecret'), {
+                    expiresIn: '24h' // expires in 24 hours
+                });
 
-  });
+                // return the information including token as JSON
+                res.json({
+                    success: true,
+                    message: 'Enjoy your token!',
+                    token
+                });
+            }
+        }
+
+    });
 });
 
 
-
+// app.use(function(req, res, next) {
+//
+//   // check header or url parameters or post parameters for token
+//   const token = req.body.token || req.query.token || req.headers['x-access-token'];
+//
+//   // decode token
+//   if (token) {
+//
+//     // verifies secret and checks exp
+//     jwt.verify(token, app.get('superSecret'), function(err, decoded) {
+//       if (err) {
+//         return res.json({ success: false, message: 'Failed to authenticate token.' });
+//       } else {
+//         // if everything is good, save to request for use in other routes
+//         req.decoded = decoded;
+//
+//         next();
+//       }
+//     });
+//
+//   } else {
+//
+//     // if there is no token
+//     // return an error
+//     return res.status(403).send({
+//         success: false,
+//         message: 'No token provided.'
+//     });
+//
+//   }
+// });
 
 
